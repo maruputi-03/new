945241c83569599bc6bb72c1eefceeba2bf3e516
maruputi-03 <mariputilohitha@gmail.com>
2025-06-04 11:21:21 +0530

    Add files via upload

---
+++ RecursionDemo1.java
@@ -0,0 +1,15 @@
+class RecursionDemo1{
+static String reverse(String str){
+if(str.isEmpty()){
+return str;
+}
+else
+{
+return reverse(str.substring(1)+str.charAt(0));
+}
+}
+public static void main(String[] args){
+String s="hello";
+System.out.println("Reversed string:"+reverse(s));
+}
+}
