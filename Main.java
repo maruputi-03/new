bc5d3f947d9beb4df0271bfbe0cce7d81f155c91
maruputi-03 <mariputilohitha@gmail.com>
2025-06-04 11:20:09 +0530

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,15 @@
+class Main{
+static int sumNaturalNumbers(int n){
+if(n==1){
+return 1;
+}
+else{
+return n+sumNaturalNumbers(n-1);
+}
+}
+public static void main(String[] args){
+int number=10;
+int result=sumNaturalNumbers(number);
+System.out.println("sum of first"+ number +"is"+result);
+}
+}
